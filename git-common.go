906c433d9f9289d2ab8d590c357d76d19f827e3a
Markus Huggler <markus.huggler@ecologic.ch>
2020-04-07 18:13:49 +0200

    chore: updates dependencies

--- git-common.go
+++ git-common.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/blang/semver"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"strings"
 )
 
