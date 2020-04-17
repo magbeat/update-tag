6e56aae9e6e42e8d19314d287ea9568c94ca5a67
Markus Huggler <markus.huggler@ecologic.ch>
2020-04-17 08:59:22 +0200

    chore: updates usage

--- cmd/update-tag/main.go
+++ cmd/update-tag/main.go
@@ -10,10 +10,10 @@ import (
 var Version = "development"
 
 func main() {
-	var vPrefix = flag.Bool("v", true, "indicates if an 'v' prefix should be added to the tags, default true")
-	var prereleasePrefix = flag.String("pre", "RC", "value of the prerelease prefix, default RC")
-	var forceDevTags = flag.Bool("forceDev", false, "forces to choose from development tags (ignoring the current branch)")
-	var forceProdTags = flag.Bool("forceProd", false, "forces to choose from production tags (ignoring the current branch)")
+	var vPrefix = flag.Bool("v", true, "indicates if an 'v' prefix should be added to the tags, defaults to true")
+	var prereleasePrefix = flag.String("pre", "RC", "value of the pre-release prefix, default is 'RC', eg. 0.1.0-RC.12")
+	var forceDevTags = flag.Bool("forceDev", false, "forces to generate tags for a pre-release (ignoring the current branch), eg. 1.0.0-RC0, 0.1.0-RC.12")
+	var forceProdTags = flag.Bool("forceProd", false, "forces to generate tags for a release (ignoring the current branch), eg. 1.0.0, 0.2.0")
 	flag.Parse()
 	if len(flag.Args()) == 1 && flag.Arg(0) == "version" {
 		printVersion()
